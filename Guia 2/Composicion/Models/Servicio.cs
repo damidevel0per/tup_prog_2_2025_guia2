cb583d8763cf93e4734113ec2386198c5826ce2d
damidevel0per <damidevtech@gmail.com>
2025-08-15 20:14:08 -0300

    subiendo clase parte composicion (incompleto)

---
+++ Guia 2/Composicion/Models/Servicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicion.Models
+{
+    internal class Servicio
+    {
+        private List<Persona> lista;
+
+        public Servicio()
+        {
+
+        }
+
+        public bool AgregarPersona(int dni, string nombre)
+        {
+            Persona nuevaPersona = new Persona(dni, nombre);
+            lista.Add(nuevaPersona);
+            return true;
+        }
+
+        public int VerCantidadPersonas()
+        {
+            int cant = lista.Count();
+            return cant;
+        }
+
+        public Persona VerPersona(int a)
+        {
+            return lista[a] as Persona;
+        }
+
+    }
+}
